835710f2b1cc32f18f2116c06b9df777209071d7
unknown <mehedi5051@gmail.com>
2020-04-12 21:36:47 +0600

    playground turned on in production

--- server.js
+++ server.js
@@ -76,7 +76,12 @@ const resolvers = {
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	playground: true,
+	introspection: true,
+});
 
 // The `listen` method launches a web server.
 const PORT = process.env.PORT || 4000;
